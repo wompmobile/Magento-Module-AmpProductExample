31c93393e85f1ca1baf6a9b1d31f2cde15f19040
alankent <alan.james.kent@gmail.com>
2017-06-16 06:18:50 +0000

    Checkpoint of controller returning template file directly so have full control over AMP page.

---
+++ Block/AmpProductBlock.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AlanKent\AmpExample\Block;
+
+class AmpProductBlock extends \Magento\Framework\View\Element\Template
+{
+    //public __construct() {
+    //}
+
+    public function getXXX() {
+        return "XXX";
+    }
+}
---
+++ Controller/Index/Index.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Product controller.
+ *
+ * Copyright c 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace AlanKent\AmpExample\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $request;
+    protected $rawResultFactory;
+
+    /**
+     * Constructor
+     * 
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Framework\App\Request\Http $request
+     * @param \Magento\Framework\Controller\Result\RawFactory $rawResultFactory,
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\App\Request\Http $request,
+        \Magento\Framework\Controller\Result\RawFactory $rawResultFactory
+    )
+    {
+        $this->rawResultFactory = $rawResultFactory;
+        $this->request = $request;
+        parent::__construct($context);
+    }
+
+    /**
+     * Execute view action
+     * 
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        /** @var \Magento\Framework\View\Layout $layout */
+        $layout = $this->_view->getLayout();
+
+        /** @var \Foo\Bar\Block\Popin\Content $block */
+        $block = $layout->createBlock(\AlanKent\AmpExample\Block\AmpProductBlock::class);
+        $block->setTemplate('AlanKent_AmpExample::'
+                            . $this->request->getControllerName() . '/'
+                            . $this->request->getActionName() . '.phtml');
+
+        $result = $this->rawResultFactory->create();
+        $result->setHeader('Content-Type', 'text/plain');
+        $result->setContents($block->toHtml());
+        return $result;
+    }
+}
